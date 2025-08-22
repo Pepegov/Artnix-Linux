54dec3ab0f90094c0c183094f96c0ff15f78369d
Pepegov <andelismore@gmail.com>
2025-08-22 20:49:56 +0300

    Fix hardware-configuration.nix not found

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo rm -rf /etc/nixos/*
 sudo cp -r ./* /etc/nixos
 sudo rm -rf /etc/nixos/.git
-sudo nixos-generate-config --dir /etc/nixos/nixos
+sudo nixos-generate-config
 sudo nixos-rebuild switch --flake .#nixos
