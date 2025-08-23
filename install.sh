8b02d5f982f8ed16b3e6048634075cfacc6c3cc2
Pepegov <andelismore@gmail.com>
2025-08-23 09:23:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disco.nix --yes-wipe-all-disks
 sudo nixos-generate-config --root /mnt
+echo "Copy configuration to /mnt/etc/nixos"
 sudo cp -r ./* /mnt/etc/nixos
 sudo rm -rf /mnt/etc/nixos/.git
 sudo nixos-rebuild switch --flake /mnt/etc/nixos#vm
