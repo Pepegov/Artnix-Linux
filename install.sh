558455c0b4bc4ae011039c1dae50dba4df332e05
Pepegov <andelismore@gmail.com>
2025-08-23 09:28:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+
+echo "${RED}Partitioning of disk space${NOCOLOR}"
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disco.nix --yes-wipe-all-disks
+echo "${RED}Generate hardware config${NOCOLOR}"
 sudo nixos-generate-config --root /mnt
-echo "Copy configuration to /mnt/etc/nixos"
+echo "${RED}Copy configuration to /mnt/etc/nixos${NOCOLOR}"
 sudo cp -r ./* /mnt/etc/nixos
 sudo rm -rf /mnt/etc/nixos/.git
+echo "${RED}Install nixos${NOCOLOR}"
 sudo nixos-rebuild switch --flake /mnt/etc/nixos#vm
